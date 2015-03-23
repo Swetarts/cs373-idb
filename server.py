52eee9053a6bc949240246d00d255106ed731d13
stv8 <stvillars8@gmail.com>
2015-03-22 20:29:02 -0500

    add people route

--- server.py
+++ server.py
@@ -14,10 +14,17 @@ cors = CORS(app)
 def index(**kwargs):
   return send_from_directory('www', 'index.html')
 
+@app.route('/<any(assets, templates,  bower_components):folder>/<path:filename>')
+def toplevel_static(folder, filename):
+  filename = safe_join(folder, filename)
+  cache_timeout = app.get_send_file_max_age(filename)
+  return send_from_directory('www', filename)
+
 @app.route('/api/characters')
 @cross_origin()
 def get_characters():
-  response = requests.get('http://www.comicvine.com/api/characters/?api_key=2a196eae09708f335bc341657e97155564ab9514&limit=10&format=json')
+  request_url = 'http://www.comicvine.com/api/characters/?api_key=2a196eae09708f335bc341657e97155564ab9514&limit=10&format=json'
+  response = requests.get(request_url)
   parsed = json.loads(response.content.decode())
   results = parsed['results']
   return json.dumps(results)
@@ -31,12 +38,14 @@ def get_character_detail(id):
   results = parsed['results']
   return json.dumps(results)
 
-
-@app.route('/<any(assets, templates,  bower_components):folder>/<path:filename>')
-def toplevel_static(folder, filename):
-  filename = safe_join(folder, filename)
-  cache_timeout = app.get_send_file_max_age(filename)
-  return send_from_directory('www', filename)
+@app.route('/api/people/')
+@cross_origin()
+def get_people(id):
+  request_url = 'http://www.comicvine.com/api/people/?api_key=2a196eae09708f335bc341657e97155564ab9514&limit=10&format=json'
+  response = requests.get(request_url)
+  parsed = json.loads(response.content.decode())
+  results = parsed['results']
+  return json.dumps(results)
 
 if __name__ == '__main__':
   app.run()
