a009625f3389d61991be6254ee06b4568b0ebd84
stv8 <stvillars8@gmail.com>
2015-03-23 16:33:02 -0500

    change color scheme

--- www/assets/javascripts/app.js
+++ www/assets/javascripts/app.js
@@ -15,7 +15,8 @@ var app = angular.module('myApp', ['ui.router', 'ui.bootstrap', 'angular-loading
     $stateProvider
       .state('home', {
         url: "/home",
-        templateUrl: "../../templates/home.html"
+        templateUrl: "../../templates/home.html",
+        controller: "HomeCtrl"
       })
       .state('about', {
         url: "/about",
--- www/assets/javascripts/directives/thumbnail_drctv.js
+++ www/assets/javascripts/directives/thumbnail_drctv.js
@@ -7,9 +7,11 @@ app.directive('thumbnail', function() {
     },
     templateUrl: '../../../templates/thumbnail.html',
     link: function(scope, element, attributes) {
-      $(element).hover(function() {
-        $('#banner-image').attr('src', scope.model.image.screen_url);
-      });
+      $(element).hover(
+        function() {
+          $('#banner-image').attr('src', scope.model.image.screen_url);
+        }
+      );
     }
   }
 });
