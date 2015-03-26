a411ad8f9115dba1a222fad10cc3527233dea2f1
Maximilian Brown <maximilian.brown@gmail.com>
2015-03-26 00:13:11 -0500

    finished db_populate_characters.py, need to test

--- db_populate_characters.py
+++ db_populate_characters.py
@@ -12,35 +12,72 @@ import json
 character_id_list = ['1807', '1440', '1696', '1486', '2349', '1698', '2048', '1469', '1443', '1699']
 
 request_url = 'http://www.comicvine.com/api/character/4005-'
-payload = {'api_key':'83ff911e240812bc29cf73246626fe319a6a4b71', 'format':'json', 'field_list':'id,name,aliases,deck,image,creators,gender,origin,powers,teams,character_friends,character_enemies', 'offset':'0'}
+payload = {'api_key':'83ff911e240812bc29cf73246626fe319a6a4b71', 'format':'json', 'field_list':'id,name,aliases,description,image,creators,gender,origin,powers,teams,character_friends,character_enemies', 'offset':'0'}
 for char_id in list(character_id_list):
     api_request = request_url + char_id + '/'
     response = requests.get(api_request, params=payload)
     num_results = response.json()['number_of_total_results']
-    response = requests.get(api_request, params=payload)
+
     parsed = response.json()['results']
-    print(parsed['image']['medium_url'])
-    # print(parsed['powers'])
-    # id = parsed['id']
-    # teams = parsed['teams']
+    
+    id = parsed['id']
     # print(id)
-    print('****************************************************************************************************')
-    # break
-    # for x in parsed:
-    #     character = Character(
-    #         id=x['id'],
-    #         name=x['name'],
-    #         alias=x['alias'],
-	 #        image=x['image']['medium_url'],
-	 #        description=,
-	 #        gender='Male',
-	 #        origin='Alien'
-    #     )
-    #     team = models.Team(id=x['id'], name=x['name'])
-    #     db.session.add(team)
-    #     db.session.commit()
 
-    # payload['offset'] = str(int(payload['offset']) + 100) 
-    # if int(payload['offset']) > int(num_results):
-    # 	break
+    name = parsed['name']
+    # print(name)
 
+    #This is ugly but some alias lists are delimited by \r\n and some just \n
+    alias = parsed['aliases'].split('\n')[0].split('\r')[0]
+    # print(alias)
+
+    images = parsed['image']
+    # print(images)
+
+    description = parsed['description']
+    # print(description)
+
+    gender = 'male' if parsed['gender'] == 1 else 'female'
+    # print(gender)
+
+    origin = parsed['origin']['name']
+    # print(origin)
+
+    #Create a new character object
+    character = models.Character(id=id, name=name, alias=alias, description=description, gender=gender, origin=origin)
+
+    #Add power objects
+    powers = parsed['powers']
+    for power in list(powers):
+        character.powers.append(models.Power.query().filter(id=power['id']).first())
+        # print(power['id'], power['name'])
+
+    #Add team objects
+    teams = parsed['teams']
+    for team in list(teams):
+        character.teams.append(models.Team.query().filter(id=team['id']).first())
+        # print(team['id'], team['name'])
+
+    #Add allies
+    allies = parsed['character_friends']
+    for ally in list(allies):
+        friend = models.Character.query.filter_by(ally['id']).first()
+        character.allies.append(friend) if friend
+        # print(ally['id'], ally['name'])
+
+    #Add enemies
+    enemies = parsed['character_enemies']
+    for enemy in list(enemies):
+        nemesis = models.Character.query.filter_by(enemy['id']).first()
+        character.enemies.append(nemesis) if nemesis
+        # print(enemy['id'], enemy['name'])
+
+    #Add creators
+    creators = parsed['creators']
+    for creator in list(creators):
+        person = models.Person.query.filter_by(creator['id']).first()
+        character.creators.append(person) if person
+        # print(creator['id'], creator['name'])
+
+    db.session.add(character)
+    db.session.commit()
+    # print('****************************************************************************************************')
