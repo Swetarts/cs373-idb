fcb1d6eb3b15e388e0a35ab828be7a0069263226
Alex Fortin <alexfortin@utexas.edu>
2015-03-25 23:02:29 -0500

    First test

--- tests.py
+++ tests.py
@@ -1,19 +1,37 @@
-import os
 import unittest
-import tempfile
+from models import Character
 from server import db
 
-class test(unittest.TestCase):
+class BuildDestroyTables(unittest.TestCase):
 
     def setUp(self):
-        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-        flaskr.app.config['TESTING'] = True
-        self.app = flaskr.app.test_client()
-        flaskr.init_db()
+        db.drop_all()
+        db.create_all()
+
 
     def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(flaskr.app.config['DATABASE'])
+        db.session.remove()
+        db.drop_all()
+
+    def test_user_setup(self):
+
+	    character = Character(
+	        id='1',
+	        name='Superman',
+	        gender='Male',
+	        alias='Clark Kent',
+	        description='The invinsible Superman',
+	        origin='Kryptonians',
+    	)
+	    db.session.add(character)
+	    db.session.commit()
+	    c = Character.query.filter_by(id='1').first()
+	    assert c.id == 1
+	    assert c.name == 'Superman'
+	    assert c.gender == 'Male'
+	    assert c.alias == 'Clark Kent'
+	    assert c.description == 'The invinsible Superman'
+	    assert c.origin == 'Kryptonians'
 
 if __name__ == '__main__':
     unittest.main()
