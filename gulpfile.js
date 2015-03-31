9c44b05c0d6fc0cdceb80f251ad45f7298b0cc30
stv8 <stvillars8@gmail.com>
2015-03-31 18:36:40 -0500

    more gulp :beer: fun

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 var gulp   = require('gulp');
 var gutil  = require('gulp-util');
 var concat = require('gulp-concat');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 // create a default task and just log a message
 gulp.task('default', function() {
@@ -12,4 +14,17 @@ gulp.task('concatjs', function() {
   return gulp.src('./www/assets/javascripts/**/*.js')
     .pipe(concat('main.js'))
     .pipe(gulp.dest('./www/dist'));
+});
+
+gulp.task('css', function() {
+  return gulp.src('./www/assets/stylesheets/*.css')
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('serve', ['css'], function() {
+  browserSync({
+    proxy: "192.168.1.34:5000"
+  });
+
+  gulp.watch('./www/assets/stylesheets/*.css', ['css']);
 });
