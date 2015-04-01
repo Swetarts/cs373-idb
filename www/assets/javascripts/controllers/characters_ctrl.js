728ffb1e62216890938ffe1a97223e0d7dfbd50e
stv8 <stvillars8@gmail.com>
2015-04-01 17:14:13 -0500

    add order button to summary pages

--- www/assets/javascripts/controllers/characters_ctrl.js
+++ www/assets/javascripts/controllers/characters_ctrl.js
@@ -4,40 +4,4 @@ app.controller('CharactersCtrl', function($scope, characters, cfpLoadingBar, $fi
 
   // comment in to debug loading bar
   //cfpLoadingBar.start();
-
-  $scope.status = {
-    isopen: false
-  };
-
-  $scope.toggleDropdown = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-    $scope.status.isopen = !$scope.status.isopen;
-  };
-
-  var orderBy = $filter('orderBy');
-
-  $scope.order = function(predicate, reverse) {
-    $scope.characters = orderBy($scope.characters, predicate, reverse); 
-    updateOrderOpt(predicate, reverse); 
-  };
-
-  $scope.orderOpt = 'None';
-
-  function updateOrderOpt(predicate, reverse) {
-    if(predicate === 'id') {
-      $scope.orderOpt = 'None';
-    }
-    else if(predicate === 'name' && reverse === false) {
-      $scope.orderOpt = 'Name Ascending';
-    }
-    else if(predicate === 'name' && reverse === true) {
-      $scope.orderOpt = 'Name Descending';
-    }
-    else {
-      $scope.orderOpt = '';
-    }
-  }
-
-
 });
