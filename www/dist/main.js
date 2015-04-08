ae4c016026fec17d471c91193cae636e753544e0
tcorley <tyler_corley@me.com>
2015-04-07 22:46:52 -0500

    Bangers font looks sexier with font-shadow 😏.
    "Form of...Super Navbar!"  - Navbar, 2015. (Styled up the navbar a bit, hover over the icon and refresh the page 😜)

--- www/dist/main.js
+++ www/dist/main.js
@@ -358,7 +358,9 @@ app.directive('navbar', function(){
     // priority: 1,
     // terminal: true,
     // scope: {}, // {} = isolate, true = child, false/undefined = no change
-    // controller: function($scope, $element, $attrs, $transclude) {},
+    controller: function($scope, $element, $attrs, $transclude) {
+        $scope.logo = '../assets/images/'+Math.floor(Math.random() * 31)+'.png';
+    },
     // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
     restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
     templateUrl: 'templates/navbar.html',
