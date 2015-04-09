895f7bda3333a89fd478283353ea2a1a2e130e9f
jergamb <ggambo_13@utexas.edu>
2015-04-09 15:46:58 -0500

    commented out the sound feature

--- www/assets/javascripts/app.js
+++ www/assets/javascripts/app.js
@@ -13,10 +13,10 @@ var app = angular.module('myApp', ['ui.router', 'ui.bootstrap', 'angular-loading
       $("html, body").animate({ scrollTop: 0 }, 200);
     });
 
-    var slap = new Audio("assets/sounds/willhelm.mp3");
-    $("body").on('click', function() {
-      slap.play();
-    });
+    // var slap = new Audio("assets/sounds/willhelm.mp3");
+    // $("body").on('click', function() {
+    //   slap.play();
+    // });
   })
 
   // change if not on local development
