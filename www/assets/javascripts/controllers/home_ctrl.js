6bd8fbc04c31b8f4399f87db9191bdb3e4a0d464
Tyler Corley <tyler_corley@me.com>
2015-04-17 13:04:04 -0500

    Finalize the videos and clean them up a bit so that it doesn't look like we're sponsored my Marvel™ or something. 📺💸

--- www/assets/javascripts/controllers/home_ctrl.js
+++ www/assets/javascripts/controllers/home_ctrl.js
@@ -1,24 +1,5 @@
 app.controller('HomeCtrl', function($scope) {
 
-	$scope.myInterval = 4000;
-
-	$scope.slides = [
-		{
-			"image":"http://www.fantastic-four.nl/marvel-superheroes-website-marvel-now.jpg"
-		},
-		{
-			"image":"http://www.bu.edu/today/files/2013/07/t_4740907330_d101750ba5_o.jpg"
-		},
-		{
-			"image":"http://media.ignimgs.com/media/ign/imgs/minisites/topN/comic-book-heroes/top100_comic_heroes_cover.jpg"
-		},
-		{
-			"image":"http://uploads.neatorama.com/images/posts/177/57/57177/1357476489-0.jpg"
-		}
-	]
-
-	$scope.video = '../assets/videos/bgvideo'+Math.floor(Math.random() * 9)+'.mp4';
-
-	
-  
+	$scope.video = '../assets/videos/bgvideo'+Math.floor(Math.random() * 8)+'.mp4';
+ 
 });
