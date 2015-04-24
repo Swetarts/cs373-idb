481eccd18d39aa6cbded2d7f235cb281cdf3fb63
jergamb <ggambo_13@utexas.edu>
2015-04-23 21:14:41 -0500

    updated stats for sean!

--- www/assets/javascripts/controllers/about_ctrl.js
+++ www/assets/javascripts/controllers/about_ctrl.js
@@ -7,7 +7,7 @@ app.controller('AboutCtrl', function($scope) {
 			"image":"http://i.imgur.com/uYwLx6k.jpg?1",
 			"description":"I am a senior about to graduate in May of this year. I like skateboarding, programming, good beer, and kickin it with my bros. After I graduate I am going to start working as a Front End Engineer for Turnitin here in Austin.",
 			"responsibilites":"I was responsible for the front end portion of our application. I set up Angular and programmed the characters, people, and issues poritons of our site along with the details pages for each of those. I also worked with Jerry to help get his feet wet in Angular where he successfully coded up the home and about pages.",
-			"num_commits":"130",
+			"num_commits":"144",
 			"num_issues":"25",
 			"num_uTests":"3"
 		},
@@ -17,7 +17,7 @@ app.controller('AboutCtrl', function($scope) {
 			"description":"Max is a Computer Science major at the University of Texas at Austin. He is married and has two children. When he isn't at work or school, he enjoys spending time with his family. Max recently started collecting comic books with his son and when they have time, Max and his son like to look for 'bad guys'.",
 			"responsibilites":"Developed the data model/ UML diagram for the database utilizing navicat data modeler. Created python scripts to automate parsing of comicvine API data and populating our server hosted database. Worked with flask models to ensure compatibility with acutal data.",
 			// "responsibilites":"Helped Tyler with the database, again.",
-			"num_commits":"39",
+			"num_commits":"42",
 			"num_issues":"19",
 			"num_uTests":"3"
 		},
@@ -26,7 +26,7 @@ app.controller('AboutCtrl', function($scope) {
 			"image":"http://i.imgur.com/vxbT1QN.jpg?1",
 			"description":"When I’m not fighting crime and I almost never am, I’m studying at the GDC. This is my last semester studying Computer Science before I graduate with a BS and INFOSEC certificate. After graduation I hope to have more time to fight crime and save the city as well as working at AlienVault as a full time Software Engineer/superhero. My superhero origin story began in El Paso, TX until I moved to Austin, TX in 2009 to begin my studies and fight crime. Criminals beware!!!",
 			"responsibilites":"My responsibilities included started my training in Angular in the front end aspect of the project. I worked with Sean to start practicing Angular and helped out with some things that later became making the home page and about page. Definitely feel a lot better with Angular than when I first started working on this project.",
-			"num_commits":"46",
+			"num_commits":"49",
 			"num_issues":"7",
 			"num_uTests":"3"
 		},
