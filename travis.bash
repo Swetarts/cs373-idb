7e33c6fff287d8646ddefe606a5f0db8e601fc0a
stv8 <stvillars8@gmail.com>
2015-04-23 20:54:29 -0500

    add file check to travis

---
+++ travis.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+check_file () {
+    if [ -f "$1" ]
+    then
+        echo "$1 found"
+    else
+        echo "$1 not found"
+        exit -1
+    fi
+}
+
+echo "Checking for all files"
+
+check_file "apiary.apib"
+check_file "IDB.log"
+check_file "models.html"
+check_file "models.py"
+check_file "tests.py"
+check_file "tests.out"
+check_file "UML.pdf"
+check_file "db_create.py"
+check_file "README.md"
