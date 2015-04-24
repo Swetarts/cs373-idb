fd8ac5ca961cfe9a5a7463e46a0c9b3978e861d4
stv8 <stvillars8@gmail.com>
2015-04-23 20:10:38 -0500

    search on home page

--- www/assets/javascripts/controllers/home_ctrl.js
+++ www/assets/javascripts/controllers/home_ctrl.js
@@ -1,5 +1,18 @@
-app.controller('HomeCtrl', function($scope) {
+app.controller('HomeCtrl', function($scope, SearchFactory, $state) {
 
 	$scope.video = '../assets/videos/bgvideo'+Math.floor(Math.random() * 8)+'.mp4';
- 
+
+  $scope.search = function() {
+    if($scope.searchQuery !== '') {
+      var query = $scope.searchQuery;
+      SearchFactory.search(query).then(
+        function(data) {
+          $state.go('search-results', {"results": data.data.results, "query": query});
+        },
+        function(error) {
+          console.log(error)
+        }
+        );
+    }
+  } 
 });
